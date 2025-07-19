b72ceba04704c07f1d704c9bf1be00c468cdabfd
teong <0522teongkarchun@gmail.com>
2025-07-19 13:09:13 +0800

    feat: connect hud with game flow

--- main.gd
+++ main.gd
@@ -7,11 +7,14 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout() -> void:
@@ -42,11 +45,9 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
 	$ScoreTimer.start()
-
-func _ready():
-	new_game()
